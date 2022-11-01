0a6ae6c70d4e226a43e1c52b505b7fc6dc5f67e2
Daniel-Gabriel.Nita <git@danieln.xyz>
2022-11-01 19:20:58 +0000

    check if string is a number

--- timestamp.js
+++ timestamp.js
@@ -1,7 +1,18 @@
 function toHumanreadableTimestamp(ms) {
+  if(ms===null)
+  {
+    return null;
+  }
+
+  let intValue = parseInt(ms);
+
+  if(isNaN(intValue))
+  {
+    throw `${ms} is not a number`;
+  }
+
   if (ms === 0) {
     return "0d 0h 0m 0s";
   }
-  return null;
 }
 module.exports = toHumanreadableTimestamp;
--- timestamp.test.js
+++ timestamp.test.js
@@ -9,3 +9,7 @@ test("when passing 0 it returns 0d 0h 0m 0s", () => {
   let result = toHumanreadableTimestamp(0);
   expect(result).toBe("0d 0h 0m 0s");
 });
+
+test("when non integer is passed exception is thrown", () => {
+  expect(() => toHumanreadableTimestamp("random string")).toThrow("random string is not a number");
+});
