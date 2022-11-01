3f89b85934c8781b1cf1e9ee42005a9fa6923bc7
E-K8 <ekatplat@gmail.com>
2022-11-01 18:44:45 +0000

    add test for 0 input

--- timestamp.js
+++ timestamp.js
@@ -1,4 +1,7 @@
-function toHumanreadableTimestamp() {
-    return null;
+function toHumanreadableTimestamp(ms) {
+  if (ms === 0) {
+    return "0d 0h 0m 0s";
+  }
+  return null;
 }
 module.exports = toHumanreadableTimestamp;
--- timestamp.test.js
+++ timestamp.test.js
@@ -1,9 +1,11 @@
-const toHumanreadableTimestamp = require('./timestamp');
+const toHumanreadableTimestamp = require("./timestamp");
 
+test("when function is called with null it returns null", () => {
+  let result = toHumanreadableTimestamp(null);
+  expect(result).toBe(null);
+});
 
-test('when function is called with null it returns null', () => {
-
-    let result = toHumanreadableTimestamp(null);
-    expect(result).toBe(null);
-})
-
+test("when passing 0 it returns 0d 0h 0m 0s", () => {
+  let result = toHumanreadableTimestamp(0);
+  expect(result).toBe("0d 0h 0m 0s");
+});
