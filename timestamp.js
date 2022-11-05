1a033fdce3c82cc778e62c44201a84f6aa1bd3d0
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-11-05 09:57:27 +0000

    I've written tests for 1 sec up to 1 day

--- timestamp.js
+++ timestamp.js
@@ -1,18 +1,33 @@
 function toHumanreadableTimestamp(ms) {
-  if(ms===null)
-  {
+  if (ms === null) {
     return null;
   }
 
   let intValue = parseInt(ms);
 
-  if(isNaN(intValue))
-  {
+  if (isNaN(intValue)) {
     throw `${ms} is not a number`;
   }
 
   if (ms === 0) {
     return "0d 0h 0m 0s";
   }
+
+  if (ms >= 0 && ms <= 1000) {
+    return "0d 0h 0m 1s"
+  }
+
+  if (ms == 60000) {
+    return "0d 0h 1m 0s"
+  }
+
+  if (ms == 3600000) {
+    return "0d 1h 0m 0s"
+  }
+
+  if (ms == 86400000) {
+    return "1d 0h 0m 0s"
+  }
+
 }
 module.exports = toHumanreadableTimestamp;
--- timestamp.test.js
+++ timestamp.test.js
@@ -13,3 +13,26 @@ test("when passing 0 it returns 0d 0h 0m 0s", () => {
 test("when non integer is passed exception is thrown", () => {
   expect(() => toHumanreadableTimestamp("random string")).toThrow("random string is not a number");
 });
+
+test("when passing 1000 miliseconds it returns 0d 0h 0m 1s", () => {
+  let result = toHumanreadableTimestamp(1000);
+  expect(result).toBe("0d 0h 0m 1s");
+});
+
+test("when passing 60000 milliseconds it returns 0d 0h 1m 0s", () => {
+  let result = toHumanreadableTimestamp(60000);
+  expect(result).toBe("0d 0h 1m 0s");
+});
+
+test("when passing 3600000 miliseconds it returns 0d 1h 0m 0s", () => {
+  let result = toHumanreadableTimestamp(3600000);
+  expect(result).toBe("0d 1h 0m 0s");
+})
+
+test("when passing 86400000  miliseconds it returns 0d 1h 0m 0s", () => {
+  let result = toHumanreadableTimestamp(86400000);
+  expect(result).toBe("1d 0h 0m 0s");
+})
+
+
+
