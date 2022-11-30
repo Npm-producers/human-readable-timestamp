d0f37f4566a557f590ed290be04068c4a2b2e82e
Daniel-Gabriel.Nita <git@danieln.xyz>
2022-11-30 17:54:10 +0000

    wip

---
+++ test/timestamp.test.ts
@@ -0,0 +1,47 @@
+import { toHumanreadableTimestamp } from '../lib/timestamp'
+
+describe('Input validation', function () {
+  it("null", () => {
+    let result = toHumanreadableTimestamp(null as any);
+    expect(result).toBe(null);
+  });
+
+  it("undefined", () => {
+    let result = toHumanreadableTimestamp(undefined as any);
+    expect(result).toBe(null);
+  });
+
+  it("non integer", () => {
+    expect(() => toHumanreadableTimestamp("random string")).toThrow("random string is not a number");
+  });
+});
+
+describe('Conversion', function () {
+  it("0 ms", () => {
+    expect(toHumanreadableTimestamp('0')).toBe("0d 0h 0m 0s");
+  });
+
+  it("one second", () => {
+    expect(toHumanreadableTimestamp('1000')).toBe("0d 0h 0m 1s");
+  });
+
+  it("one minute", () => {
+    expect(toHumanreadableTimestamp('60000')).toBe("0d 0h 1m 0s");
+  });
+
+  it("one hour", () => {
+    expect(toHumanreadableTimestamp('3600000')).toBe("0d 1h 0m 0s");
+  });
+
+  it("one day", () => {
+    expect(toHumanreadableTimestamp('86400000')).toBe("1d 0h 0m 0s");
+  });
+
+  it("one day, 2 hours, 44 minutes, 5 seconds", () => {
+    expect(toHumanreadableTimestamp('96245000')).toBe("1d 2h 44m 5s");
+  });
+
+  it("less than one second", () => {
+    expect(toHumanreadableTimestamp('999')).toBe("0d 0h 0m 0s");
+  });
+});
